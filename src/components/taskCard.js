8fa322e2ef3f6b6a0368cd36f0f50b489bf80427
i5x64BIT <vaintomer@gmail.com>
2023-08-11 15:56:58 +0300

    Add date addition

--- src/components/taskCard.js
+++ src/components/taskCard.js
@@ -1,10 +1,11 @@
+import Render from '../util/Render';
 import Storage from '../util/Storage';
 
 export default function(task){
     const card = document.createElement('div');
     card.classList = 'card';
     card.setAttribute('checked', task.isDone);
-
+    const storage = new Storage();
     card.innerHTML = `
     <h2 class="title">${task.title}</h2>
     <p class="due-date">${task.dueDate || '<button>Add Due Date</button>'}</p>
@@ -13,8 +14,19 @@ export default function(task){
     `;
     card.querySelector('.title').addEventListener('click', () => {
         const doneStatus = task.toggleDone();
-        (new Storage()).updateTask(task.id, task)
+        storage.updateTask(task.id, task)
         card.setAttribute('checked', doneStatus);
     })
+    card.querySelector('.due-date').addEventListener('click', function(){
+        const datePicker = document.createElement('input');
+        datePicker.setAttribute('type', 'date');
+        datePicker.addEventListener('change', function(e){
+            task.dueDate = this.value;
+            storage.updateTask(task.id, task);
+            (new Render).refreshPage()
+        })
+        this.appendChild(datePicker);
+    }, {once: true})
+
     return card;
 }
--- src/util/Render.js
+++ src/util/Render.js
@@ -1,4 +1,6 @@
-import form from '../components/newTaskForm'
+import form from '../components/newTaskForm';
+import activeTasks from '../pages/activeTasks';
+import completeTask from '../pages/completeTasks';
 
 export default class{
     pageContainer = document.querySelector('.page-container');
@@ -20,8 +22,19 @@ export default class{
         };
     }
     renderPage(contentFunction){
-        const pastContent = document.querySelector('.content')
+        const pastContent = document.querySelector('.content');
         if(pastContent) this.pageContainer.removeChild(pastContent);
         this.pageContainer.appendChild(contentFunction());
+    }
+    refreshPage(){
+        const currentPage = document.querySelector('.sidebar li[selected]');
+        switch (currentPage.getAttribute('name')) {
+            case 'Active Task':
+                this.renderPage(activeTasks);
+                break;
+            case 'Complete Task':
+                this.renderPage(completeTask);
+                break;
+        }
     }
 };
