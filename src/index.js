fd905f5d3da9c035c2d8aa7fee50d23744a0bf67
i5x64BIT <vaintomer@gmail.com>
2023-08-11 11:48:45 +0300

    Add navbar selected state, Modify content: grid item centering

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import './style.css';
 import newTaskForm from './components/newTaskForm';
 import Render from './util/Render';
 import activeTasks from './pages/activeTasks';
-
-
+import completeTasks from './pages/completeTasks';
 
 const render = new Render();
 render.renderPage(activeTasks);
- 
+const setSelected = function (){
+    for(let elem of document.querySelectorAll('.sidebar li')){
+        elem.removeAttribute('selected');
+    }
+    this.setAttribute('selected', '');
+} 
+
 //Form Creation
 document.querySelector('li[name="New Task"]')
-.addEventListener('click', () => {
+.addEventListener('click', function(){
     // New-Task-Form Setup
     const form = newTaskForm();
     const pageContainer = document.querySelector('.page-container');
@@ -20,4 +25,15 @@ document.querySelector('li[name="New Task"]')
         form,
         pageContainer
     )
+})
+
+document.querySelector('li[name="Active Task"]')
+.addEventListener('click', function(){
+    render.renderPage(activeTasks);
+    setSelected.call(this);
+})
+document.querySelector('li[name="Complete Task"]')
+.addEventListener('click', function(){
+    render.renderPage(completeTasks);
+    setSelected.call(this);
 })
