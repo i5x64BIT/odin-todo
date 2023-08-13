677fc91a14ecb9763b87ab2606cd7306c5baaf3f
i5x64BIT <vaintomer@gmail.com>
2023-08-13 20:46:24 +0300

    Add assets rule

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
           {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
+          },{
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
           },
         ],
     },
